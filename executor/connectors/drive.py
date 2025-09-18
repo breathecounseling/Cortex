7f72ef6757259165e534037db23a06a8c40f9377
breathecounseling <ryan@breathecounseling.com>
2025-09-18 15:50:26 -0600

    Update drive.py

--- executor/connectors/drive.py
+++ executor/connectors/drive.py
@@ -1,1 +1,9 @@
+"""
+Google Drive connector
+Will use Google API to read/write files.
+"""
+
+def upload_file(path: str):
+    print(f"[Drive] Uploading file {path} (placeholder).")
+    return True
 
