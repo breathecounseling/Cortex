f8d9c19f69b070ed4cc3abe4f8ba08c3df59947f
breathecounseling <ryan@breathecounseling.com>
2025-09-18 15:02:03 -0600

    Update self_tasks.py

--- executor/plugins/cortex/self_tasks.py
+++ executor/plugins/cortex/self_tasks.py
@@ -1,1 +1,9 @@
 
+"""
+Cortex plugin
+Tasks that tie back into the Cortex app itself.
+"""
+
+def sync_calendar():
+    print("[Cortex] Syncing calendar (placeholder).")
+    return {"status": "ok", "calendar_events": []}
